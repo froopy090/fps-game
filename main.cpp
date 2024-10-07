c281e4c2ea21cd172bac0f32c06ddc7978daf699
Francesco Canossi <canossifrancesco4@protonmail.com>
2024-10-07 21:44:51 +0200

    Added basic screen manager

--- main.cpp
+++ main.cpp
@@ -1,45 +1,83 @@
 #include "raylib.h"
 
-//Types and Structures Definition
-typedef enum GameScreen {LOGO = 0, TITLE, GAMEPLAY, ENDING} Gamescreen;
+// Types and Structures Definition
+typedef enum GameScreen { LOGO = 0, TITLE, GAMEPLAY, ENDING } Gamescreen;
 
-int main(){
-    //Initialization
-    const int screenWidth = 500;
-    const int screenHeight = 500;
-    const int fps = 60;
+int main() {
+  // Initialization
+  // ---------------------------------------------------------
+  const int screenWidth = 800;
+  const int screenHeight = 800;
+  const int fps = 60;
 
-    InitWindow(screenWidth, screenHeight, "fps");
-    SetTargetFPS(fps);
+  InitWindow(screenWidth, screenHeight, "fps");
+  SetTargetFPS(fps);
 
-    int framesCounter = 0;
+  int framesCounter = 0;
 
-    GameScreen currentScreen = LOGO;
+  GameScreen currentScreen = LOGO;
+  //--------------------------------------------------------
 
-    //Main game loop
-    while(!WindowShouldClose()){
-        //Update
-        switch(currentScreen){
-            case LOGO:
-                framesCounter++;
-                if(framesCounter > fps*4){
-                    currentScreen = TITLE;
-                }
-                break;
-            case TITLE:
+  // Main game loop
+  while (!WindowShouldClose()) {
+    // Update
+    // --------------------------------------------------------
+    switch (currentScreen) {
+    case LOGO:
+      framesCounter++;
 
-        }
+      // waiting for 4 seconds
+      if (framesCounter > fps * 4) {
+        currentScreen = TITLE;
+      }
+      break;
+    case TITLE:
+      if (IsKeyPressed(KEY_ENTER)) {
+        currentScreen = GAMEPLAY;
+      }
+      break;
+    case GAMEPLAY:
+      // TODO: update game screen variables here
+      break;
+    case ENDING:
+      // TODO: update ending variables here
+      break;
+    default:
+      break;
+    }
+    //--------------------------------------------------------
 
+    // Draw
+    // -------------------------------------------------------
+    BeginDrawing();
+    ClearBackground(BLACK);
 
-        //Draw
-        BeginDrawing();
-            ClearBackground(BLACK);
-            DrawText("epic text", 250, 250, 32, WHITE);
-        EndDrawing();
+    switch (currentScreen) {
+    case LOGO:
+      DrawText("Froopy Games", 20, 20, 40, LIGHTGRAY);
+      break;
+    case TITLE:
+      DrawText("Epic Title", 20, 20, 40, LIGHTGRAY);
+      DrawText("Press ENTER to continue", 20, 200, 40, LIGHTGRAY);
+      break;
+    case GAMEPLAY:
+      // TODO: update game screen variables here
+      DrawText("Gameplay screen", 20, 20, 40, LIGHTGRAY);
+      break;
+    case ENDING:
+      // TODO: update ending variables here
+      break;
+    default:
+      break;
     }
 
-    //De-initialization
-    CloseWindow();
+    EndDrawing();
+    //--------------------------------------------------------
+  }
 
-    return 0;
+  // De-initialization
+  // TODO: unload all loaded data (textures, fonts, audio) here
+  CloseWindow();
+
+  return 0;
 }
