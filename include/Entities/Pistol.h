a09513219da6bd7b8ada0d679f7118a32afa8935
Francesco Canossi <canossifrancesco4@protonmail.com>
2024-10-11 22:05:21 +0200

    Added Pistol class

---
+++ include/Entities/Pistol.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "raylib.h"
+#include "Utility/Timer.h"
+
+
+
+namespace Entities {
+
+struct Sprite{
+  Rectangle source;
+  Rectangle destination;
+  Vector2 origin;
+  float rotation;
+  Color tint;
+} ;
+
+typedef enum State{IDLE, FIRING, RELOADING} State;
+
+class Pistol {
+public:
+  Pistol();
+  ~Pistol();
+  void Event();
+  void Update();
+  void Draw();
+
+private:
+  Texture2D texture;
+  State state;
+  Utility::Timer timer;
+  Sprite idle;
+  Sprite firing1;
+  Sprite firing2;
+};
+} // namespace Entities
