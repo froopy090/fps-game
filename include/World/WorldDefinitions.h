e04766a68202a445f777853eb8c45f76740fef15
Francesco Canossi <canossifrancesco4@protonmail.com>
2024-10-28 22:47:59 +0100

    Moved World structs and definitions to separate file

---
+++ include/World/WorldDefinitions.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "raylib.h"
+
+namespace World {
+constexpr int TILE_SIZE = 10;
+constexpr int WALL_HEIGHT = 10;
+
+struct Plane {
+  Vector3 position;
+  Vector2 dimension;
+  Color color;
+  BoundingBox boundingBox;
+};
+
+struct Cube {
+  Vector3 position;
+  Vector3 size;
+  Color color;
+  BoundingBox boundingBox;
+};
+} // namespace World
