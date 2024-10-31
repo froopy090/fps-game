7081fb819bf6f0381491bdfca344010083a5afed
Francesco Canossi <canossifrancesco4@protonmail.com>
2024-10-31 10:19:03 +0100

    Changed room size

--- include/World/WorldDefinitions.h
+++ include/World/WorldDefinitions.h
@@ -2,11 +2,11 @@
 
 namespace World {
 // Room Objects
-constexpr int TILE_SIZE = 10;
+constexpr int TILE_SIZE = 5;
 constexpr int WALL_HEIGHT = 10;
 
 // Rooms
-constexpr int ROOM_SIZE = 5;
+constexpr int ROOM_SIZE = 25;
 
 // World Map
 constexpr int MAP_SIZE = 5;
