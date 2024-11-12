6a4df66c3e7c86d0cddf727329c43773409fa605
Francesco Canossi <canossifrancesco4@protonmail.com>
2024-11-12 14:11:47 +0100

    Added enemy states and updated weapon states

--- include/Entities/EntityDefinitions.h
+++ include/Entities/EntityDefinitions.h
@@ -19,7 +19,7 @@ struct Feelers {
   float angle;
 };
 
-struct Sprite{
+struct Sprite {
   Rectangle source;
   Rectangle destination;
   Vector2 origin;
@@ -33,5 +33,17 @@ struct Size {
   float length;
 };
 
-typedef enum State{IDLE, FIRING, RELOADING} State;
+typedef enum WeaponState {
+  WEAPON_IDLE,
+  WEAPON_FIRING,
+  WEAPON_RELOADING
+} WeaponState;
+
+typedef enum EnemyState {
+  ENEMY_IDLE,
+  ENEMY_CHASING,
+  ENEMY_ATTACKING
+} EnemyState;
+
+
 } // namespace Entities
