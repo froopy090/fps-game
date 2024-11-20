08b3fc99b010ae399fc2c9336b66e52f0079ffd4
Francesco Canossi <canossifrancesco4@protonmail.com>
2024-11-20 14:48:10 +0100

    Added Activity() function to check if timer is running

--- include/Utility/Timer.h
+++ include/Utility/Timer.h
@@ -4,10 +4,13 @@ namespace Utility {
 class Timer {
 private:
   float lifetime;
+  float startTime;
 
 public:
   void Start(float lifetime);
   bool Finished();
+  bool Active();
   void Update();
+  void PrintTime();
 };
 } // namespace Utility
