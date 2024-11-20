6529ca05a0abe7fea5a2cd74fc71021357f9fad2
Francesco Canossi <canossifrancesco4@protonmail.com>
2024-11-20 14:48:37 +0100

    Fixed enemy vision check, cleaned up by extracting collision checks to functions

--- src/World/Room001.cpp
+++ src/World/Room001.cpp
@@ -1,11 +1,89 @@
 #include "World/Room001.h"
 #include "Entities/Enemy.h"
+#include "Entities/EntityDefinitions.h"
 #include "Entities/Player.h"
 #include "Utility/Collision.h"
+#include "World/BaseObjects.h"
+#include "World/Stairs.h"
+#include "raylib.h"
 #include "raymath.h"
 #include <iostream>
 #include <math.h>
+#include <vector>
 
+// Helper Functions -------------------------------------------------------
+//
+// resets necessary player and enemy variables
+void Reset(Entities::Player *player, Entities::Enemy *enemy) {
+  // Reset
+  player->SetPlaneCollision(false);
+  enemy->SetPlaneCollision(false);
+}
+
+template <typename Object>
+void CheckCollisionObjects(Entities::Player *player, Entities::Enemy *enemy,
+                           std::vector<Object> objects) {
+  for (Object &obj : objects) {
+    if (Utility::EntityCollisionObject(player, &obj)) {
+      Utility::LockEntityAxis(player, &obj);
+    }
+    if (Utility::EntityCollisionObject(enemy, &obj)) {
+      Utility::LockEntityAxis(enemy, &obj);
+    }
+  }
+}
+
+void CheckStairsCollision(Entities::Player *player, Entities::Enemy *enemy,
+                          std::vector<World::Stairs> stairs) {
+  // Checks collision between stairs and entities
+  for (World::Stairs &stair : stairs) {
+    World::Cube stairWall = stair.GetStairWall();
+    if (Utility::EntityCollisionObject(player, &stairWall)) {
+      Utility::LockEntityAxis(player, &stairWall);
+    }
+    if (Utility::EntityCollisionObject(enemy, &stairWall)) {
+      Utility::LockEntityAxis(enemy, &stairWall);
+    }
+    std::vector<World::Cube> stairCubes = stair.GetCubeVector();
+    for (World::Cube &stairCube : stairCubes) {
+      if (Utility::EntityCollisionObject(player, &stairCube)) {
+        Utility::LockEntityAxis(player, &stairCube);
+      }
+      if (Utility::EntityCollisionObject(enemy, &stairCube)) {
+        Utility::LockEntityAxis(enemy, &stairCube);
+      }
+    }
+  }
+}
+
+// checks if enemy vision is blocked by an object
+template <typename Object>
+void CheckEnemyVision(Entities::Player *player, Entities::Enemy *enemy,
+                      std::vector<Object> &objects) {
+  for (Object &obj : objects) {
+    // finding one wall where the vision ray collides
+    if (!Utility::CanSeeTarget(enemy, player, &obj)) {
+      if (enemy->GetState() != Entities::ENEMY_IDLE) {
+        enemy->SetIdle();
+        break;
+      }
+    }
+  }
+}
+
+void CheckEnemyVisionStairs(Entities::Player *player, Entities::Enemy *enemy,
+                            std::vector<World::Stairs> &stairs) {
+  for (World::Stairs &stair : stairs) {
+    World::Cube stairWall = stair.GetStairWall();
+    if (!Utility::CanSeeTarget(enemy, player, &stairWall) &&
+        enemy->GetState() != Entities::ENEMY_IDLE) {
+      enemy->SetIdle();
+      break;
+    }
+  }
+}
+// End Helper Functions --------------------------------------------------------
+//
 namespace World {
 const int Room001::roomMatrix[ROOM_SIZE][ROOM_SIZE] = {
     {1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -61,105 +139,18 @@ Room001::Room001() {
 }
 
 void Room001::Update(Entities::Player *player, Entities::Enemy *enemy) {
-  // Reset
-  player->SetPlaneCollision(false);
-  enemy->SetPlaneCollision(false);
-  // Checks plane collision with entities
-  for (Plane &floor : floors) {
-    if (Utility::EntityCollisionObject(player, &floor)) {
-      Utility::LockEntityAxis(player, &floor);
-    }
-    if (Utility::EntityCollisionObject(enemy, &floor)) {
-      Utility::LockEntityAxis(enemy, &floor);
-    }
-  }
+  Reset(player, enemy);
 
-  // Checks wall collision with entities
-  for (Cube &wall : walls) {
-    if (Utility::EntityCollisionObject(player, &wall)) {
-      Utility::LockEntityAxis(player, &wall);
-    }
-    if (Utility::EntityCollisionObject(enemy, &wall)) {
-      Utility::LockEntityAxis(enemy, &wall);
-    }
-  }
+  // Collision checks
+  CheckCollisionObjects(player, enemy, floors);
+  CheckCollisionObjects(player, enemy, walls);
+  CheckCollisionObjects(player, enemy, columns);
+  CheckStairsCollision(player, enemy, stairs);
 
-  // checks enemy line of sight to player
-  // this must be on a separate for loop, otherwise
-  // it will interfere with other collisions
-  for (Cube &wall : walls) {
-    if (!Utility::CanSeeTarget(enemy, player, &wall)) {
-      // enemy->SetChasePlayer(false); // enemy can't see player
-      enemy->SetIdle();
-      enemy->SetHasCollided(true);
-      break;
-    } else {
-      enemy->SetChasePlayer(); // enemy can see player
-      enemy->SetHasCollided(false);
-    }
-  }
-
-  // Checks collision between large columns and entities
-  for (LargeColumn &column : columns) {
-    if (Utility::EntityCollisionObject(player, &column)) {
-      Utility::LockEntityAxis(player, &column);
-    }
-    if (Utility::EntityCollisionObject(enemy, &column)) {
-      Utility::LockEntityAxis(enemy, &column);
-    }
-  }
-
-  // check line of sight for columns
-  if (enemy->HasCollided() == false) {
-    for (LargeColumn &column : columns) {
-      if (!Utility::CanSeeTarget(enemy, player, &column)) {
-        // enemy->SetChasePlayer(false); // enemy can't see player
-        enemy->SetIdle();
-        enemy->SetHasCollided(true);
-        break;
-      } else {
-        enemy->SetChasePlayer(); // enemy can see player
-        enemy->SetHasCollided(false);
-      }
-    }
-  }
-
-  // Checks collision between stairs and entities
-  for (Stairs &stair : stairs) {
-    Cube stairWall = stair.GetStairWall();
-    if (Utility::EntityCollisionObject(player, &stairWall)) {
-      Utility::LockEntityAxis(player, &stairWall);
-    }
-    if (Utility::EntityCollisionObject(enemy, &stairWall)) {
-      Utility::LockEntityAxis(enemy, &stairWall);
-    }
-    std::vector<Cube> stairCubes = stair.GetCubeVector();
-    for (Cube &stairCube : stairCubes) {
-      if (Utility::EntityCollisionObject(player, &stairCube)) {
-        Utility::LockEntityAxis(player, &stairCube);
-      }
-      if (Utility::EntityCollisionObject(enemy, &stairCube)) {
-        Utility::LockEntityAxis(enemy, &stairCube);
-      }
-    }
-  }
-  // checks line of sight
-  // this doesn't check the stairs themselves
-  // checks the walls
-  if (enemy->HasCollided() == false) {
-    for (Stairs &stair : stairs) {
-      Cube stairWall = stair.GetStairWall();
-      if (!Utility::CanSeeTarget(enemy, player, &stairWall)) {
-        // enemy->SetChasePlayer(false); // enemy can't see player
-        enemy->SetIdle();
-        enemy->SetHasCollided(true);
-        break;
-      } else {
-        enemy->SetChasePlayer(); // enemy can see player
-        enemy->SetHasCollided(false);
-      }
-    }
-  }
+  // Enemy vision checks
+  CheckEnemyVision(player, enemy, walls);
+  CheckEnemyVision(player, enemy, columns);
+  CheckEnemyVisionStairs(player, enemy, stairs);
 }
 
 void Room001::Draw() {
@@ -176,4 +167,5 @@ void Room001::Draw() {
     stair.Draw();
   }
 }
+
 } // namespace World
