3427698f323db04e6a7129477b664851521e1f8c
Francesco Canossi <canossifrancesco4@protonmail.com>
2024-11-22 11:30:37 +0100

    Removed dependency on Room class and fixed constructor

--- include/Utility/EnemyManager.h
+++ include/Utility/EnemyManager.h
@@ -2,18 +2,21 @@
 #include "Entities/Enemy.h"
 #include "Entities/Pistol.h"
 #include "Entities/Player.h"
-#include "World/Room001.h"
 #include "vector"
 
 namespace Utility {
 class EnemyManager {
 public:
-  EnemyManager(int numberOfEnemies, Entities::Player *player,
-               Entities::Pistol *pistol, World::Room001 *room);
+  EnemyManager(int numberOfEnemies, Entities::Player *player, Entities::Pistol *pistol);
 
   void Event();
   void Update();
   void Draw();
+
+  // adds an enemy to the vector
+  void SpawnEnemy(Vector3 position);
+
+  std::vector<Entities::Enemy> *GetEnemiesVector();
 
 private:
   std::vector<Entities::Enemy> enemies;
