f5af7dc5f9013ac9d347e077c4ed082e0f98fab8
Francesco Canossi <canossifrancesco4@protonmail.com>
2024-12-04 18:14:36 +0100

    WIP: creating objects manager, similar to EnemyManager, but for the map objects

---
+++ include/Utility/WorldObjectManager.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace Utility {
+class WorldObjectManager {
+public:
+  WorldObjectManager();
+
+  void Update();
+  void Draw();
+
+private:
+  // idk something here
+};
+} // namespace Utility
