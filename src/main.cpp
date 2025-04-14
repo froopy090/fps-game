f3733b25c11e8cd7e0556324660f0650f4c448be
froopy090 <canossifrancesco4@protonmail.com>
2025-04-14 22:58:42 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,135 +1,66 @@
-#include "Entities/Enemy.h"
-#include "Entities/Pistol.h"
-#include "Entities/Player.h"
-#include "Utility/CameraHUD.h"
-#include "Utility/PlayerInfoHUD.h"
-#include "Utility/WorldObjectManager.h"
-#include "World/Room001.h"
-#include "globals.h"
-#include "raylib.h"
-#include "resource_dir.h"
-#include <dirent.h>
-#include <memory>
-#include "World/Room002.h"
-
-#define MAX_COLUMNS 20
-
-// Types and Structures Definition
-typedef enum GameScreen { LOGO = 0, TITLE, GAMEPLAY, ENDING } Gamescreen;
+#include "ecs/Component.h"
+#include "ecs/Entity.h"
+#include "ecs/Registry.h"
+#include "ecs/System.h"
+#include "ecs/ViewCameraSystem.h"
+#include <raylib.h>
 
 int main() {
-  // Initialization
-  // ---------------------------------------------------------
-  const int fps = 144;
+  // Init
+  const int screenWidth = 800;
+  const int screenHeight = 450;
 
-  InitWindow(screenWidth, screenHeight, "fps");
-  SetTargetFPS(fps);
+  InitWindow(screenWidth, screenHeight, "ECS example");
 
-  int framesCounter = 0;
+  Registry registry;
+  Entity player = registry.createEntity();
+  ViewCamera playerCamera;
+  playerCamera.camera = {0};
+  playerCamera.camera.position = (Vector3){0.0f, 2.0f, 4.0f};
+  playerCamera.camera.target = (Vector3){0.0f, 2.0f, 0.0f};
+  playerCamera.camera.up = (Vector3){0.0f, 1.0f, 0.0f};
+  playerCamera.camera.fovy = 60.0f;
+  playerCamera.camera.projection = CAMERA_PERSPECTIVE;
+  playerCamera.cameraMode = CAMERA_FIRST_PERSON;
+  registry.addComponent(player, playerCamera);
 
-  GameScreen currentScreen = LOGO;
+  DisableCursor();
+  SetTargetFPS(60);
 
-  SearchAndSetResourceDir("resources");
+  std::vector<System *> gameSystems;
+  gameSystems.push_back(new ViewCameraSystem());
 
-  // Loading Entities
-  auto player1 = std::make_unique<Entities::Player>();
-  auto pistol = std::make_unique<Entities::Pistol>(player1.get());
-
-  // Utility objects and managers
-  auto cameraHUD = std::make_unique<Utility::CameraHUD>();
-  auto playerInfoHUD = std::make_unique<Utility::PlayerInfoHUD>();
-  auto enemyManager =
-      std::make_unique<Utility::EnemyManager>(20, player1.get(), pistol.get());
-  auto objectManager = std::make_unique<Utility::WorldObjectManager>(
-      player1.get(), enemyManager.get());
-
-  // World
-  auto room = std::make_unique<World::Room002>(
-      player1.get(), enemyManager.get(), objectManager.get());
-
-  //--------------------------------------------------------
-
-  // Main game loop
+  // Game loop
   while (!WindowShouldClose()) {
-    // Events
-    // --------------------------------------------------------
-    pistol->Event();
-    player1->Event();
-    enemyManager->Event();
-
-    // --------------------------------------------------------
     // Update
-    // --------------------------------------------------------
-    switch (currentScreen) {
-    case LOGO:
-      framesCounter++;
-
-      // waiting for 4 seconds
-      // for debugging changed to 0
-      if (framesCounter > fps * 0) {
-        currentScreen = TITLE;
-      }
-      break;
-    case TITLE:
-      if (IsKeyPressed(KEY_ENTER)) {
-        currentScreen = GAMEPLAY;
-      }
-      break;
-    case GAMEPLAY:
-      pistol->Update();
-      player1->Update();
-      enemyManager->Update();
-
-      room->Update();
-      break;
-    case ENDING:
-      // TODO: update ending variables here
-      break;
-    default:
-      break;
+    for (auto &system : gameSystems) {
+      system->Update(registry);
     }
-    //--------------------------------------------------------
 
     // Draw
-    // -------------------------------------------------------
     BeginDrawing();
+
     ClearBackground(RAYWHITE);
 
-    switch (currentScreen) {
-    case LOGO:
-      DrawText("Froopy Games", 20, 20, 40, LIGHTGRAY);
-      break;
-    case TITLE:
-      DrawText("Epic Title", 20, 20, 40, LIGHTGRAY);
-      DrawText("Press ENTER to continue", 20, 200, 40, LIGHTGRAY);
-      break;
-    case GAMEPLAY:
-      room->Draw();
+    BeginMode3D(registry.getComponent<ViewCamera>(player).camera);
 
-      enemyManager->Draw();
-      player1->Draw();
-      pistol->Draw();
-
-      // HUD
-      cameraHUD->Draw(player1.get());
-      playerInfoHUD->Draw(player1.get());
-      DrawFPS(5, 5);
-
-      break;
-    case ENDING:
-      // TODO: update ending variables here
-      break;
-    default:
-      break;
-    }
-
+    DrawPlane((Vector3){0.0f, 0.0f, 0.0f}, (Vector2){32.0f, 32.0f},
+              LIGHTGRAY); // Draw ground
+    DrawCube((Vector3){-16.0f, 2.5f, 0.0f}, 1.0f, 5.0f, 32.0f,
+             BLUE); // Draw a blue wall
+    DrawCube((Vector3){16.0f, 2.5f, 0.0f}, 1.0f, 5.0f, 32.0f,
+             LIME); // Draw a green wall
+    DrawCube((Vector3){0.0f, 2.5f, 16.0f}, 32.0f, 5.0f, 1.0f,
+             GOLD); // Draw a yellow wall
+    EndMode3D();
     EndDrawing();
-    //--------------------------------------------------------
   }
 
   // De-initialization
-  // TODO: unload all loaded data (textures, fonts, audio) here
   CloseWindow();
+  for (auto &system : gameSystems) {
+    delete system;
+  }
 
   return 0;
 }
