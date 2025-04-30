c311a08cf4db2ed76fa8777499b0da9e67926c26
froopy090 <canossifrancesco4@protonmail.com>
2025-04-30 14:35:15 +0200

    Add function to load in cube

--- include/entities/EnvironmentFactory.h
+++ include/entities/EnvironmentFactory.h
@@ -1,5 +1,6 @@
-#include <Registry.h>
 #include <Component.h>
+#include <Registry.h>
 #include <raymath.h>
 
 Entity CreatePlane(Registry &registry);
+Entity CreateCube(Registry &registry);
--- src/ecs/entities/EnvironmentFactory.cpp
+++ src/ecs/entities/EnvironmentFactory.cpp
@@ -22,3 +22,26 @@ Entity CreatePlane(Registry &registry) {
                         ColliderComponent{.bounds = bounds, .isStatic = true});
   return plane;
 }
+
+Entity CreateCube(Registry &registry) {
+  Entity cube = registry.createEntity();
+
+  Model model = LoadModel("cube.obj");
+  Texture2D texture = LoadTexture("concrete.png");
+  model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
+
+  Vector3 position = {0.0f, 3.0f, 0.0f};
+  BoundingBox bounds = GetMeshBoundingBox(model.meshes[0]);
+  bounds.max = Vector3Add(position, bounds.max);
+  bounds.min = Vector3Add(position, bounds.min);
+
+  ModelComponent modelComponent;
+  modelComponent.model = model;
+  modelComponent.texture = texture;
+
+  registry.addComponent(cube, modelComponent);
+  registry.addComponent(cube, TransformComponent{.position = position});
+  registry.addComponent(cube,
+                        ColliderComponent{.bounds = bounds, .isStatic = true});
+  return cube;
+}
