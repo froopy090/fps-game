d1e54ba266108208f7a48173deae7484ca5a7053
froopy090 <canossifrancesco4@protonmail.com>
2025-05-08 16:00:20 +0200

    Update

--- include/systems/YCollisionSystem.h
+++ include/systems/YCollisionSystem.h
@@ -19,12 +19,14 @@ private:
     auto &size = registry.getComponent<SizeComponent>(collision.entity1);
 
     if (collision.axis == CollisionInfo::Axis::Y) {
-      if (collision.direction == CollisionInfo::Direction::TOP) {
+      if (collision.direction == CollisionInfo::Direction::TOP &&
+          velocity.velocity.y < 0) {
         transform.position.y = collision.box2.max.y + size.size.y;
         velocity.velocity.y = 0.0f;
       }
 
-      if (collision.direction == CollisionInfo::Direction::BOTTOM) {
+      if (collision.direction == CollisionInfo::Direction::BOTTOM &&
+          velocity.velocity.y > 0) {
         float offset = 0.9f;
         transform.position.y = collision.box2.min.y - size.size.y + offset;
         velocity.velocity.y = 0.0f;
